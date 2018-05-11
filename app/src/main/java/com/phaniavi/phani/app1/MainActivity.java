2c97372a8d3ef486dec3ad28e43784e338a29f15
phaniavi <crvphanindragupta@gmail.com>
2018-05-11 23:54:27 +0530

    Second Commit

--- app/src/main/java/com/phaniavi/phani/app1/MainActivity.java
+++ app/src/main/java/com/phaniavi/phani/app1/MainActivity.java
@@ -2,6 +2,9 @@ package com.phaniavi.phani.app1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView firstTextView = findViewById(R.id.textView);
+
+        Button detectButton = findViewById(R.id.detectButton);
+        detectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstTextView.setText(R.string.updatedText);
+            }
+        });
+
+        Button clearButton = findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstTextView.setText(R.string.clearedText);
+            }
+        });
     }
 }
